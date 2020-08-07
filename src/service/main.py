b9237e183e0333f74127400c2b0f98e00f812231
Steven HH Chen <s335276@gmail.com>
2020-08-07 16:14:45 +0800

    Add python TCP socket testing function
    Add permission INTERNET in the Makefile
    Add socket client function in the service

--- src/service/main.py
+++ src/service/main.py
@@ -1,4 +1,15 @@
-# Python for Android Service
-import logging
+# Python for Android Service - connect to 7688 so
+import socket
 
-logging.info('Service output log.')
+host='192.168.43.89'
+port=502
+
+if __name__ == '__main__':
+	so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	so.connect((host, port))
+	so.sendall(b'Hello Steven, This message is sent by Android Service.')
+
+	resp = str(so.recv(1024), encoding='utf-8')
+	print("resp: %s" % resp)
+
+	so.close()
